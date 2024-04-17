17ad1dfef53939d7c298b8947d050fd023555caa
Márcio Narciso D <marcionarciso@live.com>
2024-04-17 00:28:47 -0300

    feat: criada uma factory utilizando o padrão Factory Method para instanciar corretamente a regra de cálculo da taxa de sua respectiva forma de pagamento.

---
+++ app/Factories/RegraCalculoTaxaFactory.php
@@ -0,0 +1,43 @@
+<?php 
+
+namespace App\Factories;
+use App\Enums\FormaPagamento;
+use App\Models\RegrasCalculoTaxas\{
+    TaxaDoCredito, 
+    TaxaDoDebito, 
+    TaxaDoPix,
+    RegraCalculoTaxa
+};
+
+class RegraCalculoTaxaFactory
+{
+    private static array $regras = [
+        FormaPagamento::CREDITO->value => TaxaDoCredito::class,
+        FormaPagamento::DEBITO->value => TaxaDoDebito::class,
+        FormaPagamento::PIX->value => TaxaDoPix::class
+    ];
+
+    /**
+     * Instancia a classe da regra de cálculo da taxa com base na forma de pagamento.
+     * @param FormaPagamento $formaPagamento
+     * @return \App\Models\RegrasCalculoTaxas\RegraCalculoTaxa
+     * @throws \Exception
+     */
+    public static function make(FormaPagamento $formaPagamento) : RegraCalculoTaxa
+    {
+        /**
+         * Obtém a classe que deve ser instanciada com base na forma de pagamento.
+         */
+        $regraCalculo = self::$regras[$formaPagamento->value];
+
+        /**
+         * Se não existir a classe da regra de cálculo correspondente a forma de 
+         * pagamento, é lançada uma exceção.
+         */
+        if (empty($regraCalculo)) {
+            throw new \Exception('Forma de Pagamento inválida.');
+        }
+
+        return new $regraCalculo();
+    }
+}
