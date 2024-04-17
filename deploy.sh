94a4635ddeec719b2a7a8d0c79907bf7909afede
Márcio Narciso D <marcionarciso@live.com>
2024-04-17 09:57:33 -0300

    ops: descomentando scripts.

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 DIR_ATUAL=$(dirname $0)
 
-#exec $DIR_ATUAL/scripts/install-php.sh
+exec $DIR_ATUAL/scripts/install-php.sh
 
-#exec $DIR_ATUAL/scripts/install-mysql.sh
+exec $DIR_ATUAL/scripts/install-mysql.sh
 
-#exec $DIR_ATUAL/scripts/create-database-user.sh
+exec $DIR_ATUAL/scripts/create-database-user.sh
 
-#exec $DIR_ATUAL/scripts/install-nginx.sh
+exec $DIR_ATUAL/scripts/install-nginx.sh
 
 exec $DIR_ATUAL/scripts/prepare-project.sh
