cc2f78d02cf5f1c1249ff57d84abb7d19a447804
Márcio Narciso D <marcionarciso@live.com>
2024-04-17 20:08:45 -0300

    refat: refatorando a model da conta para realizar a transação de duas contas; agora a conta também tem soft delete.

--- app/Models/Conta.php
+++ app/Models/Conta.php
@@ -5,12 +5,52 @@ namespace App\Models;
 use App\Exceptions\ContaComSaldoInsuficienteException;
 use App\Factories\RegraCalculoTaxaFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Conta extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'id', 'saldo'
     ];
+
+    /**
+     * Subtrai do saldo da conta o valor que ela está pagando em uma transação.
+     * @param float $valor
+     * @return $this
+     * @throws \App\Exceptions\ContaComSaldoInsuficienteException
+     */
+    private function subtrairDoSaldo(float $valor) : self
+    {
+        $isContaComSaldoSuficiente = $this->saldo >= $valor;
+    
+        /**
+         * Se a conta não tiver saldo suficiente para concluir a transação,
+         * é lançada uma exceção.
+         */
+        if (! $isContaComSaldoSuficiente) {
+            throw new ContaComSaldoInsuficienteException("A conta pagadora '{$this->id}' não tem saldo suficiente para completar a transação.");
+        }
+
+        $this->saldo -= $valor;
+
+        return $this;
+    }
+
+    /**
+     * Adiciona ao saldo da conta o valor que ela está recebendo de uma transação.
+     * @param float $valor
+     * @return $this
+     */
+    private function adicionarAoSaldo(float $valor) : self
+    {
+        $this->saldo += $valor;
+
+        return $this;
+    }
 
     /**
      * Realiza a transação bancária na conta em questão.
@@ -18,33 +58,40 @@ class Conta extends Model
      * 
      * @param TransacaoBancaria $transacaoBancaria  Define o tipo de forma de pagamento e o valor transacionado.
      * @return $this
-     * @throws \Exception
+     * @throws \App\Exceptions\ContaComSaldoInsuficienteException
+     * @throws \App\Exceptions\RegraCalculoTaxaInexistenteException
      */
-    public function realizarTransacao(TransacaoBancaria $transacaoBancaria) : self
+    public static function realizarTransacao(TransacaoBancaria $transacaoBancaria) : void
     {
-        /**
-         * Calcula o valor da transação com base na taxa da forma de pagamento.
-         */
-        $valorDaTransacao = RegraCalculoTaxaFactory::make($transacaoBancaria->formaPagamento)
-                                ->calcularValorTaxa($transacaoBancaria->valor);
+        DB::transaction(function () use ($transacaoBancaria) {
 
-        /**
-         * Valor total que deve ser descontado da conta.
-         */
-        $valorTotalDaTransacao = $valorDaTransacao + $transacaoBancaria->valor;
+            /**
+             * Calcula o valor da transação com base na taxa da forma de pagamento.
+             */
+            $valorDaTransacao = RegraCalculoTaxaFactory::make($transacaoBancaria->forma_pagamento)
+                                    ->calcularValorTaxa($transacaoBancaria->valor);
+    
+            /**
+             * Valor total que deve ser descontado da conta pagadora.
+             */
+            $valorTotalDaTransacao = $valorDaTransacao + $transacaoBancaria->valor;
+    
+            /**
+             * Subtrai o valor total da transação (taxa + valor da transação) 
+             * do saldo do pagador.
+             */
+            $transacaoBancaria->pagador->subtrairDoSaldo($valorTotalDaTransacao)->save();
 
-        $hasContaSaldoSuficiente = $this->saldo >= $valorTotalDaTransacao;
+            /**
+             * Adiciona somente o valor da transação (sem taxa) ao recebedor.
+             */
+            $transacaoBancaria->recebedor->adicionarAoSaldo($transacaoBancaria->valor)->save();
 
-        /**
-         * Se a conta não tiver saldo suficiente para concluir a transação,
-         * é lançada uma exceção.
-         */
-        if (! $hasContaSaldoSuficiente) {
-            throw new ContaComSaldoInsuficienteException("A conta '{$this->id}' não tem saldo suficiente para completar a transação.");
-        }
+            /**
+             * Salva a transação no banco para histórico.
+             */
+            $transacaoBancaria->save();
 
-        $this->saldo = $this->saldo - $valorTotalDaTransacao;
-
-        return $this;
+        });
     }
 }
