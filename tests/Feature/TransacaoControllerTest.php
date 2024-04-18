d91f28bb4badd6826d42903b4f34cd82d62b4082
Márcio Narciso D <marcionarciso@live.com>
2024-04-17 21:50:07 -0300

    fix: atualizando os testes da controller de transação.

--- tests/Feature/TransacaoControllerTest.php
+++ tests/Feature/TransacaoControllerTest.php
@@ -10,15 +10,29 @@ class TransacaoControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_deve_consultar_saldo_de_uma_conta(): void
+    #[\Override]
+    protected function setUp(): void
     {
+        parent::setUp();
+
+        // Arrange
         $this->postJson('/conta',[
             'conta_id' => 1234,
             'valor' => 500.0
         ]);
 
+        $this->postJson('/conta', [
+            'conta_id' => 1235,
+            'valor' => 500.0
+        ]);
+    }
+
+    public function test_deve_consultar_saldo_de_uma_conta(): void
+    {
+        // Act
         $resp = $this->get('/conta?id=1234');
 
+        // Assert
         $resp
             ->assertStatus(200)
             ->assertJson([
@@ -29,21 +43,19 @@ class TransacaoControllerTest extends TestCase
 
     public function test_deve_efetuar_compra_no_valor_de_50_no_debito(): void
     {
+        // Arrange
         $saldoDaConta = 500.0;
-
-        $this->postJson('/conta',[
-            'conta_id' => 1234,
-            'valor' => $saldoDaConta
-        ]);
-
         $valorDaTransacao = 50.0;
 
+        // Act
         $resp = $this->postJson('/transacao', [
             'forma_pagamento' => 'D',
-            'conta_id' => 1234,
+            'pagador_id' => 1234,
+            'recebedor_id' => 1235,
             'valor' => $valorDaTransacao
         ]);
 
+        // Assert
         $resp
             ->assertStatus(201)
             ->assertJson([
@@ -55,20 +67,17 @@ class TransacaoControllerTest extends TestCase
     public function test_deve_efetuar_compra_no_valor_de_100_no_credito(): void
     {
         $saldoDaConta = 500.0;
-
-        $this->postJson('/conta',[
-            'conta_id' => 1234,
-            'valor' => $saldoDaConta
-        ]);
-
         $valorDaTransacao = 100.0;
 
+        // Act
         $resp = $this->postJson('/transacao', [
             'forma_pagamento' => 'C',
-            'conta_id' => 1234,
+            'pagador_id' => 1234,
+            'recebedor_id' => 1235,
             'valor' => $valorDaTransacao
         ]);
 
+        // Assert
         $resp
             ->assertStatus(201)
             ->assertJson([
@@ -80,20 +89,17 @@ class TransacaoControllerTest extends TestCase
     public function test_deve_realizar_uma_transferencia_no_valor_de_75_no_pix(): void
     {
         $saldoDaConta = 500.0;
-
-        $this->postJson('/conta',[
-            'conta_id' => 1234,
-            'valor' => $saldoDaConta
-        ]);
-
         $valorDaTransacao = 75.00;
 
+        // Act
         $resp = $this->postJson('/transacao', [
             'forma_pagamento' => 'P',
-            'conta_id' => 1234,
+            'pagador_id' => 1234,
+            'recebedor_id' => 1235,
             'valor' => $valorDaTransacao
         ]);
 
+        // Assert
         $resp
             ->assertStatus(201)
             ->assertJson([
@@ -104,33 +110,29 @@ class TransacaoControllerTest extends TestCase
 
     public function test_nao_deve_realizar_uma_transferencia_para_formas_de_pagamento_desconhecidas(): void
     {
-        $this->postJson('/conta',[
-            'conta_id' => 1234,
-            'valor' => 500.0
-        ]);
-
+        // Act
         $resp = $this->postJson('/transacao', [
             'forma_pagamento' => 'Y',
-            'conta_id' => 1234,
+            'pagador_id' => 1234,
+            'recebedor_id' => 1235,
             'valor' => 75.00
         ]);
 
+        // Assert
         $resp->assertStatus(400);
     }
 
     public function test_nao_deve_realizar_transferencia_com_valor_negativo(): void
     {
-        $this->postJson('/conta',[
-            'conta_id' => 1234,
-            'valor' => 500.0
-        ]);
-
+        // Act
         $resp = $this->postJson('/transacao', [
             'forma_pagamento' => 'P',
-            'conta_id' => 1234,
+            'pagador_id' => 1234,
+            'recebedor_id' => 1235,
             'valor' => -75.00
         ]);
 
+        // Assert
         $resp->assertStatus(400);
     }
 }
