19acce15fb361cad8a508a607d23d56d2361bf1d
Márcio Narciso D <marcionarciso@live.com>
2024-04-18 11:46:36 -0300

    chore: criando symlink para que o arquivo de documentação da api do Swagger seja acessível.

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,6 +71,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('docs') => storage_path('api-docs') // Permite o acesso ao arquivo .json criado pelo Swagger
     ],
 
 ];
