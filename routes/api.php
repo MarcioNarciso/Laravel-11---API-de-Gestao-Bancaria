68f0adb894cbdfdcc1c224de13416323b802cb90
Márcio Narciso D <marcionarciso@live.com>
2024-04-24 21:09:50 -0300

    fix: limitando ações não utilizadas na api da conta de resource controller.

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,15 @@ use App\Http\Controllers\ContaController;
 use App\Http\Controllers\TransacaoController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('conta', ContaController::class);
+/**
+ * "apiResource" ignora automaticamente os métodos "create" e "edit", que normalmente
+ * têm como retorno código HTML.
+ * 
+ * "only" define quais ações a controller deve tratar.
+ */
+Route::apiResource('conta', ContaController::class)->only([
+    'index', 'show', 'store', 'destroy'
+]);
 
 Route::post('/transacao', [TransacaoController::class, 'transacao'])->name('transacao');
 Route::get('/transacao/{contaId}', [TransacaoController::class, 'listarTransacoes'])->name('listarTransacoes');
