2dfd77aa34d265e488cbf3a1532fc6ad2139ad07
Márcio Narciso D <marcionarciso@live.com>
2024-05-13 14:37:22 -0300

    refactor: movendo lógica da model BankTransaction para o repositório BankTransactionRepository.

--- app/Models/BankTransaction.php
+++ app/Models/BankTransaction.php
@@ -4,8 +4,6 @@ namespace App\Models;
 
 use App\Enums\PaymentMethod;
 use App\Traits\UnchangeableModel;
-use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -51,19 +49,4 @@ class BankTransaction extends BaseModel
         return $this->belongsTo(Account::class, 'receiverId');
     }
 
-    /**
-     * Busca todas as transações que um conta está relacionada, seja como pagadora
-     * ou recebedora.
-     * 
-     * @param   \App\Models\Account $account
-     * @param   null|int            $perPage    Quantidade de registros por página.
-     * @param   null|int            $page       Número da página de registros.
-     * @return  \Illuminate\Contracts\Pagination\LengthAwarePaginator
-     */
-    public static function getAccountTransactions(Account $account, ?int $perPage = null, ?int $page) : LengthAwarePaginator
-    {
-        return BankTransaction::where('payerId', $account->id)
-                                ->orWhere('receiverId', $account->id)
-                                ->paginate($perPage, page: $page);
-    }
 }
--- app/Repositories/BankTransactionRepository.php
+++ app/Repositories/BankTransactionRepository.php
@@ -26,8 +26,19 @@ class BankTransactionRepository implements BankTransactionRepositoryInterface
         return $this;
     }
 
+    /**
+     * Busca todas as transações que um conta está relacionada, seja como pagadora
+     * ou recebedora.
+     * 
+     * @param   \App\Models\Account $account
+     * @param   null|int            $perPage    Quantidade de registros por página.
+     * @param   null|int            $page       Número da página de registros.
+     * @return  \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
     public function getAccountTransactions (Account $account, ?int $perPage = null, ?int $page = null) : LengthAwarePaginator
     {
-        return BankTransaction::getAccountTransactions($account, $perPage, $page);
+        return BankTransaction::where('payerId', $account->id)
+                                ->orWhere('receiverId', $account->id)
+                                ->paginate($perPage, page: $page);
     }
 }
