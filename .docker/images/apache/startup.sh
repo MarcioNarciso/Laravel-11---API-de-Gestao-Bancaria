62bf0591225911b25ad39e01154932cde3ae6da7
Márcio Narciso D <marcionarciso@live.com>
2024-05-16 21:08:19 -0300

    ops: conteinerização da aplicação, com Docker Compose.

---
+++ .docker/images/apache/startup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+# Instala as dependências do projeto
+php composer.phar install
+
+# Gera uma nova chave para produção
+php artisan key:generate
+
+# Gera a documentação do Swagger
+php artisan l5-swagger:generate
+
+# Otimiza os caches
+php artisan optimize
+
+echo 'Waiting for connection to the database...'
+
+# A execução do script esperará até a conexão com o banco estiver disponível
+output=`php artisan app:wait-for-mysql --t 10 --n 6`
+
+if [ $? -eq 1 ]; then
+    echo $output
+    return 1
+fi
+
+echo $output
+
+# Realiza as migrações no banco e cria o cria se não existir
+php artisan migrate --force
+
+# Inicializa o serviço do Apache
+apache2-foreground
